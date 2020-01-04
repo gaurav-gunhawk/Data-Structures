efe8e8f00ad092a11d2f7275fffed9e925f31f95
gaurav-gunhawk <47500789+gaurav-gunhawk@users.noreply.github.com>
2020-01-04 11:54:12 +0530

    Add files via upload

---
+++ Stack/Stack.java
@@ -0,0 +1,40 @@
+public class Stack {
+	public static void main(String args[])
+	{
+		StackHelper st = new StackHelper();
+		st.push(43);
+		st.push(63);
+		st.push(41);
+		st.pop();
+		st.push(76);
+		st.show();
+	}
+}
+
+class StackHelper
+{
+	int stack[] = new int[5];
+	int top = 0;
+	
+	void push(int data)
+	{
+		stack[top] = data;
+		top++;
+	}
+	
+	void pop()
+	{
+		top--;
+		int data = stack[top];
+		stack[top] = 0;
+	}
+	
+	void show()
+	{
+		for(int i=0 ; i<stack.length ; i++)
+		{
+			System.out.println(stack[i]);
+		}
+	}
+	
+}
